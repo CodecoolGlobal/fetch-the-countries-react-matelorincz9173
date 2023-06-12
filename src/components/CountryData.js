ead0e99740bef02ad8fbbfc3e237ab928bd3afe9
matelorincz9173 <lorincz.mate.to@gmail.com>
2023-06-12 17:25:12 +0200

    tidying up looks

--- src/components/CountryData.js
+++ src/components/CountryData.js
@@ -4,11 +4,13 @@ export default function CountryData({country, backClicked}) {
 
   return (
     <div>
-        <h1>{country.name.official}</h1>
-        <h2>{country.region}</h2>
-        <a href={country.maps.googleMaps}>Google Maps</a>
-        <h2>{country.population}</h2>
-        <img src={country.flags.png}></img>
+      <div>
+          <h1>{country.name.official}</h1>
+          <h2>Region:{country.region}</h2>
+          <a href={country.maps.googleMaps}>Google Maps</a>
+          <h2>Population:{country.population}</h2>
+          <img src={country.flags.png}></img>
+      </div>
         <button onClick={backClicked}>Back</button>
     </div>
   )
